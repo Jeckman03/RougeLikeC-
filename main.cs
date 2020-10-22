ad6cc01a07391e6b7cd7ce2b9e7bf9687fba2b4a
Jeff <jeckman03@gmail.com>
2020-10-22 18:58:38 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,20 @@
+/* --This is a Rougelike console game created by: Jeff Eckman Date: 10/22/20-- */
+using System;
+
+namespace ConsoleUI
+{
+
+	class MainClass {
+  public static void Main (string[] args) {
+
+
+
+    Console.ReadLine();
+  }
+
+	// Methods
+
+
+}
+}
+
